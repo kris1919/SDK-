ec56be472f313e609e9e4363121c9dde03f4007b
miaoyh <miaoyh@morningtec.cn>
2017-10-17 15:56:31 +0800

    Add static library

---
+++ Products/StaticFramework/StaticFramework.framework/Headers/StaticFramework.h
@@ -0,0 +1,19 @@
+//
+//  StaticFramework.h
+//  StaticFramework
+//
+//  Created by krisyuhao on 2017/9/30.
+//  Copyright © 2017年 krisyuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StaticFramework.
+FOUNDATION_EXPORT double StaticFrameworkVersionNumber;
+
+//! Project version string for StaticFramework.
+FOUNDATION_EXPORT const unsigned char StaticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaticFramework/PublicHeader.h>
+
+#import <StaticFramework/SFSimpleLog.h>
