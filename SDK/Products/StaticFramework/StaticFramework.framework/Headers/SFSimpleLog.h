bf2c184a706f2bd5173948484b178acb4adee8f0
miaoyh <miaoyh@morningtec.cn>
2017-10-17 15:40:11 +0800

    add file

---
+++ SDK/Products/StaticFramework/StaticFramework.framework/Headers/SFSimpleLog.h
@@ -0,0 +1,15 @@
+//
+//  SFSimpleLog.h
+//  StaticFramework
+//
+//  Created by krisyuhao on 2017/9/30.
+//  Copyright © 2017年 krisyuhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFSimpleLog : NSObject
+
++ (void)consoleString:(NSString *)string;
+
+@end
